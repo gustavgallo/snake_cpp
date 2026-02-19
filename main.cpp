475635657ba8dd73a4250c6f61973e9bbf43f9c7
Gustavo Gallo <gustavotibollagallo@gmail.com>
2026-02-19 01:02:39 -0300

    tested collision, all ok, food now being created and the end screen done too, minor bugs still exist, need to fix

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <conio.h>
 #include <windows.h>
 #include "utils/snake.hpp"
@@ -16,6 +18,16 @@ void clearScreen()
 #else
     system("clear");
 #endif
+}
+
+void createFood(pos &food)
+{
+
+    unsigned int posX = rand() % (WIDTH - 2) + 1;
+    unsigned int posY = rand() % (HEIGHT - UI_HEIGHT - 2) + (UI_HEIGHT + 1);
+
+    food.x = posX;
+    food.y = posY;
 }
 
 void getInput(snake &snake)
@@ -43,10 +55,79 @@ void getInput(snake &snake)
     }
 }
 
-int main()
+bool drawBorders(int x, int y)
 {
 
+    if ((x == 0 || x == WIDTH - 1) && y > UI_HEIGHT)
+    {
+        cout << "|";
+        return true;
+    }
+
+    if (y == UI_HEIGHT)
+    {
+        cout << "_";
+        return true;
+    }
+
+    if (y == HEIGHT - 1)
+    {
+        cout << "_";
+        return true;
+    }
+    return false;
+}
+
+void drawGameOver()
+{
+    clearScreen();
+
+    for (int y = 0; y < HEIGHT; y++)
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            // Bordas
+            if (y == 0 || y == HEIGHT - 1)
+            {
+                cout << "_";
+            }
+            else if (x == 0 || x == WIDTH - 1)
+            {
+                cout << "|";
+            }
+            else
+            {
+                // Centralizar texto
+                int centerY = HEIGHT / 2;
+                int centerX = (WIDTH / 2) - 5;
+
+                if (y == centerY && x == centerX)
+                {
+                    cout << "GAME OVER";
+                    x += 8; // pular o resto da palavra
+                }
+                else if (y == centerY + 2 && x == centerX - 4)
+                {
+                    cout << "Press any key to exit";
+                    x += 20;
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+
     snake snake(WIDTH / 2, HEIGHT / 2);
+    pos apple;
+    createFood(apple);
 
     while (true)
     {
@@ -56,50 +137,53 @@ int main()
         snake.move();
         snake.wrap(WIDTH, HEIGHT, UI_HEIGHT);
 
+        pos head = snake.getHead();
+
+        if (head.x == apple.x && head.y == apple.y)
+        {
+            snake.grow();
+            createFood(apple);
+        }
+
         clearScreen();
 
         cout << "SNAKE TEST\n";
-        cout << "Use WASD to move | Q to leave\n";
+        // cout << "Use WASD to move | Q to leave\n";
+        cout << "Apple: " << apple.x << " " << apple.y << endl;
 
         const auto &body = snake.getBody();
-        if (snake.checkSelfCollision()){
-            exit(0);
+        if (snake.checkSelfCollision())
+        {
+            drawGameOver();
+            cin.get();
+            break;
         }
 
+        // PRINTING STARTS DOWN FROM HERE
+        /////////////////////////////////
         for (int y = UI_HEIGHT; y < HEIGHT; y++)
         {
             for (int x = 0; x < WIDTH; x++)
             {
 
+                if (drawBorders(x, y))
+                    continue;
+
                 bool printed = false;
-
-                if ((x == 0 || x == WIDTH - 1) && y > UI_HEIGHT)
-                {
-                    cout << "|";
-                    continue;
-                }
-
-                if (y == UI_HEIGHT)
-                {
-                    cout << "_";
-                    continue;
-                }
-
-                if (y == HEIGHT - 1)
-                {
-                    cout << "_";
-                    continue;
-                }
-
 
                 for (int i = 0; i < body.size(); i++)
                 {
                     if (body[i].x == x && body[i].y == y)
                     {
-                        cout << (i == 0 ? '@' : '*');
+                        cout << (i == 0 ? 'O' : 'o');
                         printed = true;
                         break;
                     }
+                }
+
+                if ((apple.x == x) && (apple.y == y))
+                {
+                    cout << '*';
                 }
 
                 if (!printed)
